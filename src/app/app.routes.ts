fdff49dd5f67354811009cc447db6c2005699bcb
Mario Andres Morales <mario.morales@devsu.com>
2024-05-27 21:14:54 -0500

    menu added with styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./shared/components/layout/layout.component'),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () => import('./components/dashboard/dashboard.component')
+            },
+            {
+                path: 'profile',
+                loadComponent: () => import('./components/profile/profile.component')
+            },
+            {
+                path: 'tables',
+                loadComponent: () => import('./components/tables/tables.component')
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+
+        ]
+    }, 
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
